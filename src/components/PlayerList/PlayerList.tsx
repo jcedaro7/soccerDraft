2686df20e11ce925bae1a0c891764e5f519239e5
Nicolas Lobos <68791728+cocolobos@users.noreply.github.com>
2022-01-16 11:36:47 -0300

    SD-05 Added drawer navigation (#4)
    Co-authored-by: Franco Marini <franco.marini@climate.com>

---
+++ src/components/PlayerList/PlayerList.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Image, View, Text,ScrollView, StyleSheet, SafeAreaView } from 'react-native';
+import Header from '../SharedComponents/Header/Header';
+import user from '../../../assets/user.png';
+
+const PlayerList = () => {
+  const players = [
+    {
+      id: 1,
+      name: "Nicolas Lobos",
+      nickName: "Coco"
+    },
+    {
+      id: 2,
+      name: "Julian Cedaro",
+      nickName: "Yulian"
+    }
+  ];
+  return(
+    <SafeAreaView style={styles.container}>
+      <Header headerTitle={'Player List'} />
+      <ScrollView>
+        {
+          players.map((player) =>   {
+            return (
+              <View style={styles.playerContainer} key={player.id}>
+                <View>
+                  <Image source={user} style={{ width: 50, height: 50, margin: 10 }} />
+                </View>
+                <View>
+                  <Text style={styles.playerName}>{player.name}</Text>
+                  <Text style={styles.playerNickName}>{player.nickName}</Text>
+                </View>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
+    </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width:'100%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#000000'
+  },
+  playerContainer: {
+    height: 100,
+    width: 400,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#0000',
+    borderBottomWidth: 1,
+    borderColor: "#fff"
+  },
+  playerLogo: {
+    width: 50,
+    height: 50,
+    margin: 10
+  },
+  playerName: {
+    marginBottom: 10,
+    color: '#fff',
+  },
+  playerNickName: {
+    fontFamily: 'Arial',
+    color: 'red',
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+
+export default PlayerList;
